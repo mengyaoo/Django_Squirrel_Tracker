1be04bc357ad2e6e5cede90c83e5b44d248b8846
mengyaoo <mh4048@columbia.edu>
2020-05-03 02:34:26 +0000

    change setting

--- squirrelsite/settings.py
+++ squirrelsite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_squirrel_data',
+    'sightings',
 ]
 
 MIDDLEWARE = [
