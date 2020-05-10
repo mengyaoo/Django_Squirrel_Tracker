ff7301e929f9a2ec45b87a9ddd54664957bb9d93
mengyaoo <mh4048@columbia.edu>
2020-05-10 03:38:21 +0000

    add templates and edit views

--- map/views.py
+++ map/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import sys,os,inspect
+from sightings.models import squirrelsite
 
+
+def index(reques):
+    squirrels = squirrelsite.objects.all()
+    sightings = squirrels[:100]
+    context={
+            'sightings':sightings,
+            }
+    return render(request,'map/map.html',context)
 # Create your views here.
